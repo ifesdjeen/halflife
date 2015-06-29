fe44e51f3da9914391feb9fcd959804b0e5d93ac
Alex Petrov <oleksandr.petrov@gmail.com>
2015-06-29 18:26:38 +0200

    Add AVar

---
+++ src/java/halflife/bus/concurrent/AVar.java
@@ -0,0 +1,36 @@
+package halflife.bus.concurrent;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Haskell-like Atomic Variable.
+ */
+public class AVar<T> {
+
+  private final CountDownLatch     latch;
+  private final AtomicReference<T> ref;
+
+  public AVar() {
+    this.latch = new CountDownLatch(1);
+    this.ref = new AtomicReference<T>();
+  }
+
+  public void set(T obj) {
+    if (this.ref.compareAndSet(null, obj)) {
+      this.ref.set(obj);
+      this.latch.countDown();
+    } else {
+      throw new RuntimeException("This AVar has already been set");
+    }
+  }
+
+  public T get(long timeout, TimeUnit unit) throws InterruptedException {
+    if (this.latch.await(timeout, unit)) {
+      return this.ref.get();
+    } else {
+      throw new RuntimeException("AVar never retrieved a value");
+    }
+  }
+}
