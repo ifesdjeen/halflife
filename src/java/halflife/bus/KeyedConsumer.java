e831139e731d5cab8781cf872ecdeed003fa4a93
Alex Petrov <oleksandr.petrov@gmail.com>
2015-06-29 18:38:05 +0200

    Implement default

--- src/java/halflife/bus/KeyedConsumer.java
+++ src/java/halflife/bus/KeyedConsumer.java
@@ -4,6 +4,9 @@ import reactor.fn.Consumer;
 
 public interface KeyedConsumer<T> extends Consumer<T> {
 
+  public default void accept(T value) {
+    // No op
+  }
   public default void accept(Object k, T value) {
     accept(value);
   }
