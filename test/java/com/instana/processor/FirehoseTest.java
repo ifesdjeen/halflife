a6e4db35102544745c36d557636ac14c6798ce69
Alex Petrov <oleksandr.petrov@gmail.com>
2015-06-30 22:33:50 +0200

    Write tests

--- test/java/com/instana/processor/FirehoseTest.java
+++ test/java/com/instana/processor/FirehoseTest.java
@@ -4,7 +4,7 @@ import halflife.bus.Firehose;
 import halflife.bus.KeyedConsumer;
 import halflife.bus.concurrent.AVar;
 import halflife.bus.registry.ConcurrentRegistry;
-import halflife.bus.registry.Registry;
+import halflife.bus.registry.DefaultingRegistry;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,15 +17,14 @@ import reactor.fn.tuple.Tuple2;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class FirehoseTest {
 
-  private Dispatcher                      dispatcher;
-  private Registry<Object, KeyedConsumer> consumerRegistry;
-  private Consumer<Throwable>             dispatchErrorHandler;
-  private Firehose                        firehose;
+  private Dispatcher                                         dispatcher;
+  private DefaultingRegistry<String, KeyedConsumer<Integer>> consumerRegistry;
+  private Consumer<Throwable>                                dispatchErrorHandler;
+  private Firehose<String, Integer>                          firehose;
 
   @Before
   public void setup() {
@@ -36,9 +35,9 @@ public class FirehoseTest {
       throwable.printStackTrace();
     };
 
-    this.firehose = new Firehose(dispatcher,
-                                 consumerRegistry,
-                                 dispatchErrorHandler);
+    this.firehose = new Firehose<>(dispatcher,
+                                   consumerRegistry,
+                                   dispatchErrorHandler);
   }
 
   @After
@@ -49,29 +48,41 @@ public class FirehoseTest {
 
   @Test
   public void simpleOnTest() throws InterruptedException {
-    AVar val = new AVar();
-    firehose.on("key1", new KeyedConsumer() {
-      @Override
-      public void accept(Object o) {
-        val.set(o);
-      }
-    });
+    AVar<Integer> val = new AVar<>();
+    AVar<Integer> val2 = new AVar<>();
+
+    firehose.on("key1", val::set);
+    firehose.on("key2", val2::set);
+
     firehose.notify("key1", 1);
+    firehose.notify("key2", 2);
 
     assertThat(val.get(10, TimeUnit.MILLISECONDS), is(1));
+    assertThat(val2.get(10, TimeUnit.MILLISECONDS), is(2));
   }
 
   @Test
   public void simpleOn2Test() throws InterruptedException {
-    AVar<Tuple2> val = new AVar();
-    firehose.on("key1", new KeyedConsumer() {
-      @Override
-      public void accept(Object key, Object value) {
-        val.set(Tuple.of(key, value));
-      }
+    AVar<Tuple2> val = new AVar<>();
+    firehose.on("key1", (key, value) -> {
+      val.set(Tuple.of(key, value));
     });
     firehose.notify("key1", 1);
 
+    assertThat(val.get(10, TimeUnit.MILLISECONDS), is(Tuple.of("key1", 1)));
+  }
+
+  @Test
+  public void keyMissTest() throws InterruptedException {
+    AVar<Tuple2> val = new AVar<>();
+
+    firehose.miss((k_) -> true, () -> {
+      return (key, value) -> {
+        val.set(Tuple.of(key, value));
+      };
+    });
+
+    firehose.notify("key1", 1);
     assertThat(val.get(10, TimeUnit.MILLISECONDS), is(Tuple.of("key1", 1)));
   }
 }
