a84fee7145954f18f67342bddc344363df624981
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-04 15:20:18 +0200

    Add anonymous stream test

---
+++ test/java/com/instana/processor/AnonymousStreamTest.java
@@ -0,0 +1,32 @@
+package com.instana.processor;
+
+import halflife.bus.Stream;
+import halflife.bus.concurrent.AVar;
+import halflife.bus.key.Key;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class AnonymousStreamTest extends AbstractFirehoseTest {
+
+  @Test
+  public void testMap() throws InterruptedException {
+    Stream<Integer> stream = new Stream<>(firehose);
+    AVar<Integer> res = new AVar<>();
+
+    stream.map(Key.wrap("source"), (i) -> i + 1)
+          .map((i -> i * 2))
+          .consume(res::set);
+
+
+    firehose.notify(Key.wrap("source"), 1);
+
+    assertThat(res.get(1, TimeUnit.SECONDS), is(4));
+
+  }
+
+
+}
