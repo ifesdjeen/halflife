3420f58b1301113e38fbbca8f9fee91be5d6159d
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-04 02:18:08 +0200

    Add wrap to simple consumer

--- src/java/halflife/bus/SimpleConsumer.java
+++ src/java/halflife/bus/SimpleConsumer.java
@@ -1,19 +1,17 @@
 package halflife.bus;
 
+import halflife.bus.key.Key;
+
 @FunctionalInterface
-public interface SimpleConsumer<T> {
+public interface SimpleConsumer<V> {
 
-  public void accept(T value);
+  public void accept(V value);
 
-  public default void accept(Object k, T value) {
-    accept(value);
-  }
-
-  public static <T> KeyedConsumer<T> wrap(SimpleConsumer<T> c) {
-    return new KeyedConsumer<T>() {
+  public static <T> KeyedConsumer<?, T> wrap(SimpleConsumer<T> consumer) {
+    return new KeyedConsumer<Object, T>() {
       @Override
       public void accept(Object k, T value) {
-        c.accept(value);
+        consumer.accept(value);
       }
     };
   }
