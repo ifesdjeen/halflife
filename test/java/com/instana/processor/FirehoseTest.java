f7d9943039af9e1ec7c035210e2d8596e272e9ac
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-04 21:42:52 +0200

    Fix tests

--- test/java/com/instana/processor/FirehoseTest.java
+++ test/java/com/instana/processor/FirehoseTest.java
@@ -1,17 +1,8 @@
 package com.instana.processor;
 
-import halflife.bus.Firehose;
-import halflife.bus.KeyedConsumer;
 import halflife.bus.concurrent.AVar;
 import halflife.bus.key.Key;
-import halflife.bus.registry.ConcurrentRegistry;
-import halflife.bus.registry.DefaultingRegistry;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import reactor.core.Dispatcher;
-import reactor.core.dispatch.SynchronousDispatcher;
-import reactor.fn.Consumer;
 import reactor.fn.tuple.Tuple;
 import reactor.fn.tuple.Tuple2;
 
@@ -45,20 +36,22 @@ public class FirehoseTest extends AbstractFirehoseTest {
     });
     firehose.notify(Key.wrap("key1"), 1);
 
-    assertThat(val.get(10, TimeUnit.MILLISECONDS), is(Tuple.of("key1", 1)));
+    assertThat(val.get(10, TimeUnit.MILLISECONDS),
+               is(Tuple.of(Key.wrap("key1"), 1)));
   }
 
   @Test
   public void keyMissTest() throws InterruptedException {
     AVar<Tuple2> val = new AVar<>();
 
-    firehose.miss((k_) -> true, () -> {
-      return (key, value) -> {
-        val.set(Tuple.of(key, value));
-      };
-    });
+    firehose.miss((k_) -> true,
+                  () -> {
+                    return (key, value) -> {
+                      val.set(Tuple.of(key, value));
+                    };
+                  });
 
     firehose.notify(Key.wrap("key1"), 1);
-    assertThat(val.get(10, TimeUnit.MILLISECONDS), is(Tuple.of("key1", 1)));
+    assertThat(val.get(10, TimeUnit.MILLISECONDS), is(Tuple.of(Key.wrap("key1"), 1)));
   }
 }
