7834af232616d498ce1a4bd45126a91ced88e84e
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-04 21:42:07 +0200

    Make registry atomic

--- src/java/halflife/bus/registry/ConcurrentRegistry.java
+++ src/java/halflife/bus/registry/ConcurrentRegistry.java
@@ -1,24 +1,26 @@
 package halflife.bus.registry;
 
+import halflife.bus.concurrent.Atom;
+import org.pcollections.HashTreePMap;
+import org.pcollections.PMap;
+import org.pcollections.PVector;
+import org.pcollections.TreePVector;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Supplier;
 
 public class ConcurrentRegistry<K, V> implements DefaultingRegistry<K, V> {
 
-  private final Map<Object, List<Registration<K, ? extends V>>> lookupMap;
-  private final Map<KeyMissMatcher<K>, Supplier<? extends V>>   keyMissMatchers;
-  private final ReentrantLock                                   lock;
+  private final Atom<PMap<Object, PVector<Registration<K, ? extends V>>>> lookupMap;
+  private final Map<KeyMissMatcher<K>, Supplier<? extends V>>             keyMissMatchers;
 
   public ConcurrentRegistry() {
-    this.lookupMap = new ConcurrentHashMap<>();
+    this.lookupMap = new Atom<>(HashTreePMap.empty());
     this.keyMissMatchers = new ConcurrentHashMap<>();
-    this.lock = new ReentrantLock();
-
   }
 
   public void addKeyMissMatcher(KeyMissMatcher<K> matcher,
@@ -28,63 +30,73 @@ public class ConcurrentRegistry<K, V> implements DefaultingRegistry<K, V> {
 
   @Override
   public Registration<K, V> register(K obj, V handler) {
-    final List<Registration<K, ? extends V>> lookedUpArr = lookupMap.get(obj);
+    final PVector<Registration<K, ? extends V>> lookedUpArr = lookupMap.deref().get(obj);
 
     if (lookedUpArr == null) {
-      final List<Registration<K, ? extends V>> emptyArr = new ArrayList<>();
       final SimpleRegistration<K, V> reg = new SimpleRegistration<>(obj,
                                                                     handler,
-                                                                    reg1 -> lookupMap.get(obj).remove(handler));
-      emptyArr.add(reg);
+                                                                    // TODO: FIX UNREGISTER
+                                                                    reg1 -> lookupMap.deref()
+                                                                                     .get(obj)
+                                                                                     .remove(handler));
+      final PVector<Registration<K, ? extends V>> emptyArr = TreePVector.singleton(reg);
 
-      lookupMap.put(obj, emptyArr);
+      lookupMap.swap(old -> old.plus(obj, emptyArr));
+
       return reg;
+
     } else {
-      final Registration<K, V> reg = new SimpleRegistration<>(obj, handler,
-                                                              reg1 -> lookupMap.get(obj).remove(reg1));
-      lookedUpArr.add(reg);
-      lookupMap.put(obj, lookedUpArr);
+      final Registration<K, V> reg = new SimpleRegistration<>(obj,
+                                                              handler,
+                                                              // TODO: FIX REMOVES!!
+                                                              reg1 -> lookupMap.deref()
+                                                                               .get(obj)
+                                                                               .remove(reg1));
+      lookupMap.swap(old -> old.plus(obj, old.get(obj).plus(reg)));
       return reg;
     }
+
   }
 
   @Override
   public boolean unregister(K key) {
-    boolean res = lookupMap.containsKey(key);
-
-    if (res) {
-      lookupMap.remove(key);
-    }
-
-    return res;
+    //    boolean res = lookupMap.containsKey(key);
+    //
+    //    if (res) {
+    //      lookupMap.remove(key);
+    //    }
+    // TODO: FIXME
+    return false;
   }
 
   @Override
   public List<Registration<K, ? extends V>> select(final K key) {
-    lookupMap.computeIfAbsent(key, (k_) -> {
-      final List<Registration<K, ? extends V>> acc = new ArrayList<>();
-      keyMissMatchers.entrySet()
-                            .stream()
-                            .filter((m) -> m.getKey().test(key))
-                            .forEach((matcher) ->  {
-                              V handler = matcher.getValue().get();
-                              acc.add(new SimpleRegistration<>(key,
-                                                               handler,
-                                                               reg ->  acc.remove(handler)));
+    return lookupMap.swap(old -> {
+      if (old.containsKey(key)) {
+        return old;
+      } else {
+        final List<Registration<K, ? extends V>> acc = new ArrayList<>();
+        keyMissMatchers.entrySet()
+                       .stream()
+                       .filter((m) -> m.getKey().test(key))
+                       .forEach((matcher) -> {
+                         V handler = matcher.getValue().get();
+                         acc.add(new SimpleRegistration<>(key,
+                                                          handler,
+                                                          // TODO: FIX REMOVES!!!
+                                                          reg -> acc.remove(handler)));
 
-                            });
-      return acc;
-    });
-
-
-    return lookupMap.get(key);
+                       });
+        return old.plus(key, TreePVector.from(acc));
+      }
+    }).get(key);
   }
 
 
   @Override
   public void clear() {
-    this.lookupMap.clear();
-    this.keyMissMatchers.clear();
+    //    this.lookupMap.clear();
+    //    this.keyMissMatchers.clear();
   }
 
 
