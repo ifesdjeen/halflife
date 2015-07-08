32991fc45b6a5e8a13426177abc50e6391a2c689
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-08 23:04:24 +0200

    Implement filter

--- src/java/halflife/bus/Stream.java
+++ src/java/halflife/bus/Stream.java
@@ -10,14 +10,13 @@ import halflife.bus.state.StatefulSupplier;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.function.Predicate;
 
 public class Stream<V> {
 
   private final Firehose firehose;
   private final StateProvider stateProvider;
 
-  // Stream subscribes to some key
   public Stream(Firehose firehose) {
     this.firehose = firehose;
     this.stateProvider = new DefaultStateProvider();
@@ -65,6 +64,22 @@ public class Stream<V> {
       @Override
       public void accept(SRC key, V value) {
         firehose.notify(destination, mapper.apply(value));
+      }
+    });
+
+    return new Stream<>(firehose);
+  }
+
+  @SuppressWarnings(value = {"unchecked"})
+  public <SRC extends Key, DST extends Key> Stream<V> filter(SRC source,
+                                                             DST destination,
+                                                             Predicate<V> predicate) {
+    firehose.on(source, new KeyedConsumer<SRC, V>() {
+      @Override
+      public void accept(SRC key, V value) {
+        if(predicate.test(value)) {
+          firehose.notify(destination, value);
+        }
       }
     });
 
