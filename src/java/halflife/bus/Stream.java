22dfbeedf057f610be17b09c03dacaefb60362f6
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-10 22:58:42 +0200

    Get rid of types in stream

--- src/java/halflife/bus/Stream.java
+++ src/java/halflife/bus/Stream.java
@@ -6,7 +6,11 @@ import halflife.bus.registry.KeyMissMatcher;
 import halflife.bus.state.DefaultStateProvider;
 import halflife.bus.state.StateProvider;
 import halflife.bus.state.StatefulSupplier;
+import org.pcollections.PVector;
+import org.pcollections.TreePVector;
 
+import java.util.List;
+import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -14,7 +18,7 @@ import java.util.function.Predicate;
 
 public class Stream<V> {
 
-  private final Firehose firehose;
+  private final Firehose      firehose;
   private final StateProvider stateProvider;
 
   public Stream(Firehose firehose) {
@@ -57,9 +61,9 @@ public class Stream<V> {
 
   @SuppressWarnings(value = {"unchecked"})
   public <SRC extends Key, DST extends Key, V1, ST> Stream<V1> map(SRC source,
-                                                               DST destination,
-                                                               BiFunction<Atom<ST>, V, V1> fn,
-                                                               ST init) {
+                                                                   DST destination,
+                                                                   BiFunction<Atom<ST>, V, V1> fn,
+                                                                   ST init) {
     Atom<ST> st = stateProvider.makeAtom(source, init);
 
     firehose.on(source, new KeyedConsumer<SRC, V>() {
@@ -96,7 +100,7 @@ public class Stream<V> {
     firehose.on(source, new KeyedConsumer<SRC, V>() {
       @Override
       public void accept(SRC key, V value) {
-        if(predicate.test(value)) {
+        if (predicate.test(value)) {
           firehose.notify(destination, value);
         }
       }
@@ -106,11 +110,11 @@ public class Stream<V> {
   }
 
   @SuppressWarnings(value = {"unchecked"})
-  public <SRC extends Key> void consume(SRC source,
-                                        Consumer<V> consumer) {
-    firehose.on(source, new KeyedConsumer<SRC, V>() {
+  public <SRC extends Key, V1> void consume(SRC source,
+                                            Consumer<V1> consumer) {
+    firehose.on(source, new KeyedConsumer<SRC, V1>() {
       @Override
-      public void accept(SRC key_, V value) {
+      public void accept(SRC key_, V1 value) {
         consumer.accept(value);
       }
     });
@@ -124,10 +128,10 @@ public class Stream<V> {
   @SuppressWarnings(value = {"unchecked"})
   public <SRC extends Key> MatchedStream<V> matched(KeyMissMatcher<SRC> keyMatcher) {
     MatchedStream<V> downstream = new MatchedStream<>();
-    firehose.miss(keyMatcher, downstream.subscribers(this));
+    firehose.miss(keyMatcher,
+                  (Function) downstream.subscribers(this));
     return downstream;
   }
-
 
 
   public <SRC extends Key> void notify(SRC src, V v) {
