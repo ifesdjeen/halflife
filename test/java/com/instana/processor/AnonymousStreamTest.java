24a81d301d9ad576fbadb8b5e9a4e287a3d4897b
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-12 18:50:16 +0200

    Add notification test

--- test/java/com/instana/processor/AnonymousStreamTest.java
+++ test/java/com/instana/processor/AnonymousStreamTest.java
@@ -1,5 +1,6 @@
 package com.instana.processor;
 
+import halflife.bus.AnonymousStream;
 import halflife.bus.Stream;
 import halflife.bus.concurrent.AVar;
 import halflife.bus.key.Key;
@@ -26,8 +27,23 @@ public class AnonymousStreamTest extends AbstractFirehoseTest {
     firehose.notify(Key.wrap("source"), 1);
 
     assertThat(res.get(1, TimeUnit.SECONDS), is(4));
-
   }
 
+
+  @Test
+  public void testNotify() throws InterruptedException {
+    Stream<Integer> stream = new Stream<>(firehose);
+    AVar<Integer> res = new AVar<>();
+
+    AnonymousStream<Integer> s = stream.anonymous(Key.wrap("source"));
+
+    s.map((i) -> i + 1)
+     .map(i -> i * 2)
+     .consume(res::set);
+
+    firehose.notify(Key.wrap("source"), 1);
+
+    assertThat(res.get(1, TimeUnit.SECONDS), is(4));
+  }
 
 }
