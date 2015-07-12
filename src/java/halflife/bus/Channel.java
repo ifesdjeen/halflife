afbec60f08cf3bf011e9a852fb803b69b4e77648
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-12 18:49:58 +0200

    Implement Channel fully

--- src/java/halflife/bus/Channel.java
+++ src/java/halflife/bus/Channel.java
@@ -5,40 +5,57 @@ import org.pcollections.PVector;
 import reactor.fn.tuple.Tuple;
 import reactor.fn.tuple.Tuple2;
 
-import java.util.function.Consumer;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 
 public class Channel<T> {
 
   private final AnonymousStream<T> stream;
-  private final Atom<PVector<T>> state;
+  private final Atom<PVector<T>>   state;
+  private final AtomicBoolean      isDrained;
 
   Channel(AnonymousStream<T> stream,
           Atom<PVector<T>> state) {
     this.stream = stream;
     this.state = state;
+    this.isDrained = new AtomicBoolean(false);
     stream.consume(e -> {
-      state.swap(old -> old.plus(e));
-    });
-  }
-
-  public T get() {
-    return state.swapReturnOther(new Function<PVector<T>, Tuple2<PVector<T>, T>>() {
-      @Override
-      public Tuple2<PVector<T>, T> apply(PVector<T> buffer) {
-        T t = buffer.get(0);
-        if(t == null) {
-          return null;
-        } else {
-          return Tuple.of(buffer.subList(1, buffer.size() - 1),
-                          t);
-        }
-
+      if (!isDrained.get()) {
+        state.swap(old -> old.plus(e));
       }
     });
   }
 
-//  public void tell(T item) {
-//    stream.notify(item);
-//  }
+  public T get() {
+    if (isDrained.get()) {
+      throw new RuntimeException("Channel is already being drained by the stream.");
+    }
+
+    return state.swapReturnOther(new Function<PVector<T>, Tuple2<PVector<T>, T>>() {
+      @Override
+      public Tuple2<PVector<T>, T> apply(PVector<T> buffer) {
+        if (buffer.size() == 0) {
+          return Tuple.of(buffer,
+                          null);
+        }
+
+        T t = buffer.get(0);
+        if (t == null) {
+          return null;
+        } else {
+          return Tuple.of(buffer.subList(1, buffer.size()),
+                          t);
+        }
+      }
+    });
+  }
+
+  public AnonymousStream<T> stream() {
+    this.isDrained.set(true);
+    return this.stream;
+  }
+
+  public void tell(T item) {
+    stream.notify(item);
+  }
 }
