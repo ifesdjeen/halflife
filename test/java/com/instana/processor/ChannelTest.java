abd8b0c946bd875bc86e126b25f1c8f958fda76d
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-12 18:50:27 +0200

    Add tests for channel

---
+++ test/java/com/instana/processor/ChannelTest.java
@@ -0,0 +1,65 @@
+package com.instana.processor;
+
+import halflife.bus.Channel;
+import halflife.bus.Stream;
+import halflife.bus.concurrent.AVar;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class ChannelTest extends AbstractFirehoseTest {
+
+  @Test
+  public void simpleChannelTest() {
+    Stream<Integer> stream = new Stream<>(firehose);
+    Channel<Integer> chan = stream.channel();
+
+    chan.tell(1);
+    chan.tell(2);
+
+    assertThat(chan.get(), is(1));
+    assertThat(chan.get(), is(2));
+    assertTrue(chan.get() == null);
+
+    chan.tell(3);
+    assertThat(chan.get(), is(3));
+  }
+
+  @Test
+  public void channelStreamTest() throws InterruptedException {
+    AVar<Integer> res = new AVar<>();
+    Stream<Integer> stream = new Stream<>(firehose);
+    Channel<Integer> chan = stream.channel();
+
+    chan.stream().consume((i) -> res.set(i));
+
+    chan.tell(1);
+
+    assertThat(res.get(1, TimeUnit.SECONDS), is(1));
+  }
+
+  @Test
+  public void drainedChannelTest() throws InterruptedException {
+    AVar<Integer> res = new AVar<>();
+    Stream<Integer> stream = new Stream<>(firehose);
+    Channel<Integer> chan = stream.channel();
+
+    chan.stream().consume((i) -> res.set(i));
+
+    chan.tell(1);
+
+    assertThat(res.get(1, TimeUnit.SECONDS), is(1));
+    Exception expectedException = null;
+    try {
+      chan.get();
+    } catch (Exception e) {
+      expectedException = e;
+    }
+    assertTrue(expectedException != null);
+  }
+}
