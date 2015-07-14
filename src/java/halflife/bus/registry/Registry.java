ec1f5dd2be470137c148dc13cc58636eff4cac00
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-14 22:56:24 +0200

    Add stream interface to registry

--- src/java/halflife/bus/registry/Registry.java
+++ src/java/halflife/bus/registry/Registry.java
@@ -1,15 +1,20 @@
 package halflife.bus.registry;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
-public interface Registry<K, V> extends Iterable<Registration<K, V>> {
+public interface Registry<K, V> extends Iterable<Registration<K, ? extends V>> {
 
   Registration<K, V> register(K sel, V obj);
 
   boolean unregister(K key);
 
+  boolean unregister(Predicate<K> key);
+
   List<Registration<K, ? extends V>> select(K key);
 
   void clear();
 
+  public Stream<Registration<K, ? extends V>> stream();
 }
