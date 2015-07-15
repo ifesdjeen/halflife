224ef8a84a8c00fc0dac81b85e6af3889d3ef194
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-15 23:18:18 +0200

    Cover partition and slide with tests

--- test/java/com/instana/processor/AnonymousStreamTest.java
+++ test/java/com/instana/processor/AnonymousStreamTest.java
@@ -5,7 +5,10 @@ import halflife.bus.Stream;
 import halflife.bus.concurrent.AVar;
 import halflife.bus.key.Key;
 import org.junit.Test;
+import org.pcollections.TreePVector;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -30,6 +33,49 @@ public class AnonymousStreamTest extends AbstractFirehoseTest {
     assertThat(res.get(1, TimeUnit.SECONDS), is(4));
   }
 
+  @Test
+  public void testPartition() throws InterruptedException {
+    Stream<Integer> stream = new Stream<>(firehose);
+    AVar<List<Integer>> res = new AVar<>();
+
+    stream.anonymous(Key.wrap("source"))
+          .partition((i) -> {
+            return i.size() == 5;
+          })
+          .consume(res::set);
+
+    stream.notify(Key.wrap("source"), 1);
+    stream.notify(Key.wrap("source"), 2);
+    stream.notify(Key.wrap("source"), 3);
+    stream.notify(Key.wrap("source"), 4);
+    stream.notify(Key.wrap("source"), 5);
+    stream.notify(Key.wrap("source"), 6);
+    stream.notify(Key.wrap("source"), 7);
+
+    assertThat(res.get(1, TimeUnit.SECONDS), is(TreePVector.from(Arrays.asList(1, 2, 3, 4, 5))));
+  }
+
+  @Test
+  public void testSlide() throws InterruptedException {
+    Stream<Integer> stream = new Stream<>(firehose);
+    AVar<List<Integer>> res = new AVar<>(6);
+
+    stream.anonymous(Key.wrap("source"))
+          .slide(i -> {
+            return i.subList(i.size() > 5 ? i.size() - 5 : 0,
+                             i.size());
+          })
+          .consume(res::set);
+
+    stream.notify(Key.wrap("source"), 1);
+    stream.notify(Key.wrap("source"), 2);
+    stream.notify(Key.wrap("source"), 3);
+    stream.notify(Key.wrap("source"), 4);
+    stream.notify(Key.wrap("source"), 5);
+    stream.notify(Key.wrap("source"), 6);
+
+    assertThat(res.get(1, TimeUnit.SECONDS), is(TreePVector.from(Arrays.asList(2,3,4,5,6))));
+  }
 
   @Test
   public void testNotify() throws InterruptedException {
