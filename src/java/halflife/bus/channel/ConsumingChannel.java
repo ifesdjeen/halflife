9d4a78e06141b0fa260cae47bd2ecb8367469c72
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-15 23:10:36 +0200

    Extend interface of ConsumingChannel

--- src/java/halflife/bus/channel/ConsumingChannel.java
+++ src/java/halflife/bus/channel/ConsumingChannel.java
@@ -2,9 +2,12 @@ package halflife.bus.channel;
 
 import halflife.bus.AnonymousStream;
 
+import java.util.concurrent.TimeUnit;
+
 public interface ConsumingChannel<T> {
 
   public T get();
+  public T get(int time, TimeUnit timeUnit) throws InterruptedException;
   public AnonymousStream<T> stream();
 
 }
