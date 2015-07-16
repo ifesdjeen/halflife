f8c30887b64a3b3a4b7579b53c431b4193408a8d
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-16 19:43:39 +0200

    Add more tests to matched streams

--- src/java/halflife/bus/MatchedStream.java
+++ src/java/halflife/bus/MatchedStream.java
@@ -1,12 +1,19 @@
 package halflife.bus;
 
+import halflife.bus.concurrent.Atom;
 import halflife.bus.key.Key;
+import halflife.bus.operation.PartitionOperation;
+import halflife.bus.operation.SlidingWindowOperation;
+import org.pcollections.PVector;
+import org.pcollections.TreePVector;
 
 import javax.lang.model.type.NullType;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
 public class MatchedStream<V> extends FinalizedMatchedStream<V> {
 
@@ -30,6 +37,62 @@ public class MatchedStream<V> extends FinalizedMatchedStream<V> {
         };
       }
     });
+    return new MatchedStream<>(suppliers);
+  }
+
+  @SuppressWarnings(value = {"unchecked"})
+  public MatchedStream<V> filter(Predicate<V> predicate) {
+    this.suppliers.add(new StreamSupplier<V, V>() {
+      @Override
+      public <SRC extends Key, DST extends Key> KeyedConsumer<SRC, V> get(SRC src,
+                                                                          DST dst,
+                                                                          Stream<V> stream) {
+        return (key, value) -> {
+          if (predicate.test(value)) {
+            stream.notify(dst, value);
+          }
+        };
+      }
+    });
+
+    return new MatchedStream<>(suppliers);
+  }
+
+  @SuppressWarnings(value = {"unchecked"})
+  public MatchedStream<List<V>> slide(UnaryOperator<List<V>> drop) {
+    this.suppliers.add(new StreamSupplier<V, V>() {
+      @Override
+      public <SRC extends Key, DST extends Key> KeyedConsumer<SRC, V> get(SRC src,
+                                                                          DST dst,
+                                                                          Stream<V> stream) {
+        Atom<PVector<V>> buffer = stream.stateProvider().makeAtom(src, TreePVector.empty());
+
+        return new SlidingWindowOperation<SRC, DST, V>(stream.firehose(),
+                                                       buffer,
+                                                       drop,
+                                                       dst);
+      }
+    });
+
+    return new MatchedStream<>(suppliers);
+  }
+
+  @SuppressWarnings(value = {"unchecked"})
+  public MatchedStream<List<V>> partition(Predicate<List<V>> emit) {
+    this.suppliers.add(new StreamSupplier<V, V>() {
+      @Override
+      public <SRC extends Key, DST extends Key> KeyedConsumer<SRC, V> get(SRC src,
+                                                                          DST dst,
+                                                                          Stream<V> stream) {
+        Atom<PVector<V>> buffer = stream.stateProvider().makeAtom(src, TreePVector.empty());
+
+        return new PartitionOperation<SRC, DST, V>(stream.firehose(),
+                                                   buffer,
+                                                   emit,
+                                                   dst);
+      }
+    });
+
     return new MatchedStream<>(suppliers);
   }
 
