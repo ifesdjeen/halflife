a932ecd9a973154b487be597486825bc5ba1e5ae
Alex Petrov <oleksandr.petrov@gmail.com>
2015-07-30 22:07:35 +0200

    Simplify downstream

--- halflife-kafka/test/java/halflife/KafkaDownstream.java
+++ halflife-kafka/test/java/halflife/KafkaDownstream.java
@@ -10,33 +10,26 @@ import java.util.function.Function;
 /**
  * Publishes all the received values to the given kafka topic
  */
-public class KafkaDownstream<K, K1, V, V1> implements Downstream<K, V> {
+public class KafkaDownstream<K, V> implements Downstream<K, V> {
 
-  private final KafkaProducer<K1, V1> kafkaProducer;
-  private final String                topic;
-  private final Function<K, K1>       keyMapper;
-  private final Function<V, V1>       valueMapper;
-  private final Function<K1, Integer> partitioner;
+  private final KafkaProducer<K, V>  kafkaProducer;
+  private final String               topic;
+  private final Function<K, Integer> partitioner;
 
   public KafkaDownstream(Properties producerProperties,
                          String topic,
-                         Function<K, K1> keyMapper,
-                         Function<V, V1> valueMapper,
-                         Function<K1, Integer> partitioner) {
+                         Function<K, Integer> partitioner) {
     this.kafkaProducer = new KafkaProducer<>(producerProperties);
     this.topic = topic;
-    this.keyMapper = keyMapper;
-    this.valueMapper = valueMapper;
     this.partitioner = partitioner;
   }
 
   @Override
   public void accept(K key, V value) {
-    K1 transformedKey = keyMapper.apply(key);
-    ProducerRecord<K1, V1> record = new ProducerRecord<>(topic,
-                                                         partitioner.apply(transformedKey),
-                                                         transformedKey,
-                                                         valueMapper.apply(value));
+    ProducerRecord<K, V> record = new ProducerRecord<>(topic,
+                                                       partitioner.apply(key),
+                                                       key,
+                                                       value);
     kafkaProducer.send(record);
   }
 }
