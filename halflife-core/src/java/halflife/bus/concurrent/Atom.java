34b759c1ea532a266884bdc6bdfe06f1815dd2cb
Alex Petrov <oleksandr.petrov@gmail.com>
2015-08-06 17:06:44 +0200

    Add predicate to atom

--- halflife-core/src/java/halflife/bus/concurrent/Atom.java
+++ halflife-core/src/java/halflife/bus/concurrent/Atom.java
@@ -3,7 +3,9 @@ package halflife.bus.concurrent;
 import reactor.fn.tuple.Tuple2;
 
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.LockSupport;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
 /**
@@ -47,6 +49,21 @@ public class  Atom<T> {
       Tuple2<T, O> newvtuple = swapOp.apply(old);
       if(ref.compareAndSet(old, newvtuple.getT1())) {
         return newvtuple.getT2();
+      }
+      LockSupport.parkNanos(1L);
+    }
+  }
+
+  public <O> O swapReturnOther(Predicate<T> pred,
+                               Function<T, Tuple2<T,O>> swapOp) {
+    for (;;) {
+      T old = ref.get();
+      if (pred.test(old)) {
+        Tuple2<T, O> newvtuple = swapOp.apply(old);
+        if (ref.compareAndSet(old, newvtuple.getT1())) {
+          return newvtuple.getT2();
+        }
+        LockSupport.parkNanos(1L); //TODO: Maybe park everywhere?
       }
     }
   }
